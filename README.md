2de37dfd8abfa30c4886067f43b7f5f4ddc2e2a7
Sophie Mann <sklm@live.co.uk>
2020-05-06 12:56:02 +0100

    Build 5 dice

--- README.md
+++ README.md
@@ -1,31 +1,14 @@
-![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
+# 5 Dice 🎲
 
+Yahtzee & Liars Dice app.
 
-# Dicee 🎲
+Roll all 5 dice or roll each dice individually.
 
-## Our Goal
+---
 
-The objective of this tutorial is to introduce you to the core programming concepts that will form the foundation of most of the apps you’ll build in the future. This app will teach you how to make apps with functionality using setState() inside Stateful Flutter widgets.
+- Stateful widgets used to update the user interface.
+- onPressed listeners used to detect when buttons are pressed.
+- setState used to mark the widget tree as dirty, requiring update on the next render.
+- Expanded widget used to make widgets adapt to screen dimensions.
 
-
-## What you will create
-
-We’re going to make a Las Vegas dice app. You can make the die roll at the press of a button. With this app in your pocket, you’ll be able to settle any score on the go!
-
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/dicee-demo.gif)
-
-## What you will learn
-
-- How to use Flutter stateless widgets to design the user interface.
-- How to use Flutter stateful widgets to update the user interface.
-- How to change the properties of various widgets.
-- How to use onPressed listeners to detect when buttons are pressed.
-- How to use setState to mark the widget tree as dirty and requiring update on the next render.
-- How to use Expanded to make widgets adapt to screen dimensions.
-- Understand and use string interpolation.
-- Learn about basic dart programming concepts such as data types and functions.
-- Code and use gesture controls.
-
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
-
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
+> Adapted from a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(
+            child: Text('5 Dice'),
+          ),
+          backgroundColor: Colors.teal,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,134 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int topLeftDiceNumber;
+  int midLeftDiceNumber;
+  int bottomDiceNumber;
+  int topRightDiceNumber;
+  int midRightDiceNumber;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  topLeftDiceNumber = Random().nextInt(6) + 1;
+                  midLeftDiceNumber = Random().nextInt(6) + 1;
+                  bottomDiceNumber = Random().nextInt(6) + 1;
+                  topRightDiceNumber = Random().nextInt(6) + 1;
+                  midRightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Text(
+                'Roll All',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        topLeftDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset(
+                      'images/dice$topLeftDiceNumber.png',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        topRightDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset(
+                      'images/dice$topRightDiceNumber.png',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        midLeftDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset(
+                      'images/dice$midLeftDiceNumber.png',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        midRightDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset(
+                      'images/dice$midRightDiceNumber.png',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        bottomDiceNumber = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset(
+                      'images/dice$bottomDiceNumber.png',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
+    );
   }
 }
